23a541c41d03d1d30ff94531fc86edab89c09ca0
Mikhail Abramishvili <mg@abramishvili.net>
2024-03-12 20:20:03 +0400

    Init

--- src/components/postavkas/Postavka.vue
+++ src/components/postavkas/Postavka.vue
@@ -4,18 +4,39 @@
             <h4 class="m-0">Поставка №{{ postavka.doc_number }}</h4>
         </div>
         <div class="col-12 col-lg-6 text-end">
-             <button v-if="postavka.status === 0" @click="provod()" class="btn btn-primary">
-                 Провести документ
-             </button>
+            <button
+                v-if="postavka.status === 0"
+                @click="del()"
+                class="btn btn-sm btn-danger"
+                :disabled="!saveButton">
+                Удалить документ
+            </button>
+
+            <button
+                v-if="postavka.status === 0"
+                @click="provod()"
+                class="btn btn-sm btn-primary ms-2"
+                :disabled="!saveButton">
+                Провести документ
+            </button>
+
+            <button
+                v-if="postavka.status === 1"
+                @click="unprovod()"
+                class="btn btn-sm btn-warning"
+                :disabled="!saveButton">
+                Отменить проведение
+            </button>
         </div>
     </div>
 
-    <Loader v-if="loader" />
+    <Loader v-if="loader"/>
 
     <div v-if="!loader" class="card mb-4">
         <div class="card-body">
             <p>Контрагент: {{ postavka.contragent.name }}</p>
             <p>Дата: {{ $filters.date(postavka.date) }}</p>
+            <p class="mb-0">Статус: {{ postavka.status === 1 ? 'проведено' : 'создано' }}</p>
         </div>
     </div>
 
@@ -32,13 +53,41 @@
             </tr>
             </thead>
             <tbody>
-                <tr v-for="item in postavka.items">
-                    <td>{{item.nomenclature.type}}</td>
-                    <td>{{item.nomenclature.title}}</td>
-                    <td>{{item.nomenclature.vendor_code}}</td>
-                    <td>{{item.nomenclature.cost_price}}</td>
-                    <td>{{item.quantity}}</td>
-                </tr>
+            <tr v-for="item in postavka.items">
+                <td>{{ item.nomenclature.type }}</td>
+                <td>{{ item.nomenclature.title }}</td>
+                <td>{{ item.nomenclature.vendor_code }}</td>
+                <td>{{ item.nomenclature.cost_price }}</td>
+                <td style="width: 150px;">
+                    <span v-if="!editor">{{ item.quantity }}</span>
+
+                    <input
+                        v-if="editor === item"
+                        v-model="item.quantity" type="number"
+                        class="form-control form-control-sm">
+                </td>
+                <td class="text-end">
+                    <template v-if="postavka.status === 0">
+                        <button
+                            v-if="!editor"
+                            @click="editItem(item)"
+                            class="btn btn-sm btn-outline-warning me-2">
+                            <i class="tf-icons ti ti-edit"></i>
+                        </button>
+                        <button
+                            v-if="editor"
+                            @click="updateItem(item)"
+                            class="btn btn-sm btn-outline-success me-2">
+                            <i class="tf-icons ti ti-check"></i>
+                        </button>
+                        <button
+                            @click="delItem(item)"
+                            class="btn btn-sm btn-outline-danger">
+                            <i class="tf-icons ti ti-trash"></i>
+                        </button>
+                    </template>
+                </td>
+            </tr>
             </tbody>
         </table>
     </div>
@@ -51,6 +100,8 @@ export default {
             postavka: {},
 
             loader: true,
+            saveButton: true,
+            editor: null,
         }
     },
     created() {
@@ -66,12 +117,67 @@ export default {
                     this.loader = false
                 })
         },
+        editItem(item) {
+            this.editor = item
+        },
+        updateItem(item) {
+            axios
+                .put(`${import.meta.env.VITE_API_SERVER}/api/ff-postavka-item/${item.id}/update`, {
+                    quantity: item.quantity
+                })
+                .then(response => {
+                    this.loadPostavka()
+
+                    this.editor = null
+                })
+        },
+        delItem(item) {
+            if(this.postavka.items.length === 1) {
+                return this.$toast.error('Документ поставки не может быть пустой')
+            }
+
+            if (confirm('Точно удалить запись?')) {
+                axios
+                    .delete(`${import.meta.env.VITE_API_SERVER}/api/ff-postavka-item/${item.id}/delete`)
+                    .then(response => {
+                        this.loadPostavka()
+                    })
+            }
+        },
         provod() {
+            this.saveButton = false
+
             axios
                 .get(`${import.meta.env.VITE_API_SERVER}/api/ff-postavka/${this.$route.params.uuid}/provod`)
                 .then(response => {
-                    this.$router.push({name: 'Postavkas'})
+                    this.loadPostavka()
+
+                    this.saveButton = true
+
+                    this.$toast.success('Документ проведен')
                 })
+        },
+        unprovod() {
+            this.saveButton = false
+
+            axios
+                .get(`${import.meta.env.VITE_API_SERVER}/api/ff-postavka/${this.$route.params.uuid}/unprovod`)
+                .then(response => {
+                    this.loadPostavka()
+
+                    this.saveButton = true
+
+                    this.$toast.success('Проведение документа отменено')
+                })
+        },
+        del() {
+            if(confirm('Точно удалить документ?')) {
+                axios
+                    .delete(`${import.meta.env.VITE_API_SERVER}/api/ff-postavka/${this.$route.params.uuid}/delete`)
+                    .then(response => {
+                        this.$router.push({name: 'Postavkas'})
+                    })
+            }
         },
     },
 }
