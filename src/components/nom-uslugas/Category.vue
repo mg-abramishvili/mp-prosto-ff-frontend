3587b212a21ba053835f664310da1f9880c18d72
Mikhail Abramishvili <mg@abramishvili.net>
2024-04-17 00:18:55 +0400

    .

--- src/components/nom-uslugas/Category.vue
+++ src/components/nom-uslugas/Category.vue
@@ -1,73 +1,45 @@
 <template>
-    <div class="row align-items-center mb-4">
-        <div class="col-12 col-lg-6">
-            <h4 class="py-3 m-0">{{ category.name ?? '-' }}</h4>
-        </div>
-        <div class="col-12 col-lg-6 text-end">
+    <div>
+        <button @click="toggleCategory(category.id)" class="tree-list-item">
+            +
+            {{ category.name }}
+        </button>
 
-        </div>
-    </div>
-
-    <div class="card folder-list">
-        <div class="card-body">
-            <Loader v-if="views.loading"/>
-
-            <div v-if="category.children && category.children.length" class="row">
-                <div v-for="childCategory in category.children" class="col-12 col-lg-3">
-                    <RouterLink :to="{name: 'NomUslugaCategory', params: {id: childCategory.id}}" class="folder-list-item">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96" fill="#FFF">
-                            <path fill="#FFB900" d="M45,24l-4.2426-4.2426C39.6321,18.6321,38.106,18,36.5147,18H9c-1.6569,0-3,1.3431-3,3v56 c0,0.5523,0.4477,1,1,1h82c0.5523,0,1-0.4477,1-1V27c0-1.6569-1.3431-3-3-3H45z"/>
-                            <path fill="#FFD75E" d="M45,24l-4.2426,4.2426C39.6321,29.3679,38.106,30,36.5147,30H6v47c0,0.5523,0.4477,1,1,1h82 c0.5523,0,1-0.4477,1-1V27c0-1.6569-1.3431-3-3-3H45z"/>
-                            <linearGradient id="a" x1="48" x2="48" y1="24" y2="78" gradientUnits="userSpaceOnUse">
-                                <stop offset="0" stop-color="#fff" stop-opacity="0"/>
-                                <stop offset="1" stop-color="#ffd75e" stop-opacity=".3"/>
-                            </linearGradient>
-                            <path fill="url(#a)" d="M45,24l-4.2426,4.2426C39.6321,29.3679,38.106,30,36.5147,30H6v47c0,0.5523,0.4477,1,1,1h82 c0.5523,0,1-0.4477,1-1V27c0-1.6569-1.3431-3-3-3H45z"/>
-                            <path opacity=".4" d="M6,30v1h30.6005c1.8565,0,3.637-0.7375,4.9497-2.0503L46.5,24H45l-4.2426,4.2426 C39.6321,29.3679,38.106,30,36.5147,30H6z"/>
-                            <path fill="#DA7B16" d="M89,78H7c-0.5523,0-1-0.4477-1-1l0,0h84l0,0C90,77.5523,89.5523,78,89,78z"/>
-                        </svg>
-
-                        <span>
-                            {{ childCategory.name }}
-                        </span>
-                    </RouterLink>
-                </div>
-            </div>
-
-            <div v-if="category.uslugas && category.uslugas.length" class="row mt-4">
-                <div v-for="usluga in category.uslugas" class="col-12 col-lg-3">
-                    <a href="#">
-                        {{usluga.name}}
-                    </a>
-                </div>
-            </div>
-        </div>
+        <template v-for="childCategory in category.children">
+            <tree
+                v-if="opened.categories.includes(childCategory.id)"
+                :category="childCategory"
+            />
+        </template>
     </div>
 </template>
 
 <script>
-
 export default {
+    props: ['category'],
+    name: 'tree',
     data() {
         return {
-            category: {},
-
-            views: {
-                loading: true,
-            }
+            opened: {
+                categories: [],
+            },
         }
     },
-    created() {
-        this.loadCategory()
-    },
     methods: {
-        loadCategory() {
-            axios.get(`${import.meta.env.VITE_API_FF_SERVER}/api/nom-usluga-category/${this.$route.params.id}`)
+        loadCategory(id) {
+            axios
+                .get(`${import.meta.env.VITE_API_FF_SERVER}/api/nom-usluga-category/${id}`)
                 .then(response => {
-                    this.category = response.data
-
-                    this.views.loading = false
+                    this.opened.categories.push(response.data.id)
                 })
+        },
+        toggleCategory(id) {
+            console.log(this.opened.categories)
+            if(this.opened.categories.includes(id)) {
+                this.opened.categories = this.opened.categories.filter(c => c !== id)
+            } else {
+                this.loadCategory(id)
+            }
         },
     },
 }
--- src/components/nom-uslugas/Index.vue
+++ src/components/nom-uslugas/Index.vue
@@ -4,40 +4,24 @@
             <h4 class="py-3 m-0">Услуги</h4>
         </div>
         <div class="col-12 col-lg-6 text-end">
-<!--            <RouterLink :to="{name: 'NomUslugaMaster'}" class="btn btn-primary ms-2">-->
-<!--                Добавить-->
-<!--            </RouterLink>-->
+            <!--            <RouterLink :to="{name: 'NomUslugaMaster'}" class="btn btn-primary ms-2">-->
+            <!--                Добавить-->
+            <!--            </RouterLink>-->
         </div>
     </div>
 
-    <div v-if="!views.loading && categories.length" class="card folder-list">
+    <div v-if="!views.loading && categories.length" class="card tree-list">
         <div class="card-body">
-            <div class="row">
-                <div v-for="category in categories" class="col-12 col-lg-3">
-                    <RouterLink :to="{name: 'NomUslugaCategory', params: {id: category.id}}" class="folder-list-item">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96" fill="#FFF">
-                            <path fill="#FFB900" d="M45,24l-4.2426-4.2426C39.6321,18.6321,38.106,18,36.5147,18H9c-1.6569,0-3,1.3431-3,3v56 c0,0.5523,0.4477,1,1,1h82c0.5523,0,1-0.4477,1-1V27c0-1.6569-1.3431-3-3-3H45z"/>
-                            <path fill="#FFD75E" d="M45,24l-4.2426,4.2426C39.6321,29.3679,38.106,30,36.5147,30H6v47c0,0.5523,0.4477,1,1,1h82 c0.5523,0,1-0.4477,1-1V27c0-1.6569-1.3431-3-3-3H45z"/>
-                            <linearGradient id="a" x1="48" x2="48" y1="24" y2="78" gradientUnits="userSpaceOnUse">
-                                <stop offset="0" stop-color="#fff" stop-opacity="0"/>
-                                <stop offset="1" stop-color="#ffd75e" stop-opacity=".3"/>
-                            </linearGradient>
-                            <path fill="url(#a)" d="M45,24l-4.2426,4.2426C39.6321,29.3679,38.106,30,36.5147,30H6v47c0,0.5523,0.4477,1,1,1h82 c0.5523,0,1-0.4477,1-1V27c0-1.6569-1.3431-3-3-3H45z"/>
-                            <path opacity=".4" d="M6,30v1h30.6005c1.8565,0,3.637-0.7375,4.9497-2.0503L46.5,24H45l-4.2426,4.2426 C39.6321,29.3679,38.106,30,36.5147,30H6z"/>
-                            <path fill="#DA7B16" d="M89,78H7c-0.5523,0-1-0.4477-1-1l0,0h84l0,0C90,77.5523,89.5523,78,89,78z"/>
-                        </svg>
-
-                        <span>
-                            {{ category.name }}
-                        </span>
-                    </RouterLink>
-                </div>
-            </div>
+            <tree
+                v-for="category in categories"
+                :category="category"
+            />
         </div>
     </div>
 </template>
 
 <script>
+import tree from "./Category.vue"
 
 export default {
     data() {
@@ -46,9 +30,6 @@ export default {
 
             views: {
                 loading: true,
-                table: {
-                    settings: false,
-                },
             }
         }
     },
@@ -65,6 +46,9 @@ export default {
                     this.views.loading = false
                 })
         },
+    },
+    components: {
+        tree,
     },
 }
 </script>
